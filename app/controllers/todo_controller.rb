d22e7a3921f8d938e74a806ef5e7978dfe4a7cb9
S Douglas <sareenah.douglas@nyu.edu>
2017-09-26 01:40:45 +0000

    Added link to index

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,7 +2,47 @@ class TodoController < ApplicationController
   def index
   end
   def show
+     todo_id = params[:id]
+     
+     if todo_id == '1'
       @todo_description = "Make the curriculum"
       @todo_pomodoro_estimate = 4
+      
+     else if todo_id == '2'
+      @todo_description = "Buy workshop supplies"
+      @todo_pomodoro_estimate = 3
+      
+      else if todo_id == '3'
+      @todo_description = "Meet with the volunteer trainers "
+      @todo_pomodoro_estimate = 2
+      
+      else if todo_id == '4'
+      @todo_description = "Order food for Saturday and Sunday"
+      @todo_pomodoro_estimate = 1
+      
+      else if todo_id == '5'
+      @todo_description = "Check pre-work assignments"
+      @todo_pomodoro_estimate = 2
+      
+      else if todo_id == '6'
+      @todo_description = "Check pre-work assignments"
+      @todo_pomodoro_estimate = 2
+      
+      else if todo_id == '7'
+      @todo_description = "Check pre-work assignments"
+      @todo_pomodoro_estimate = 2
+      
+      else
+      @todo_description = "Work is Completed"
+      @todo_pomodoro_estimate = 0
+     
+    
+     end
+     end
+     end 
+     end 
+     end 
+     end
+     end
   end
 end
